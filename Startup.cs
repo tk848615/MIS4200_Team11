8f979790c2692b766dd75c5e39f3f27579fea725
Tim Kieltyka <tk848615@ohio.edu>
2020-02-24 11:13:55 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200_Team11.Startup))]
+namespace MIS4200_Team11
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
