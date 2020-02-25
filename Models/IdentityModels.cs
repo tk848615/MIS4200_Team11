da94b16e789a005eadefe7d4a2f8243c98b1e1e3
Tim Kieltyka <tk848615@ohio.edu>
2020-02-25 17:19:35 -0500

    Change 2 2/25/2020

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -30,6 +30,6 @@ namespace MIS4200_Team11.Models
             return new ApplicationDbContext();
         }
 
-        public System.Data.Entity.DbSet<MIS4200_Team11.Models.ApplicationUser> ApplicationUsers { get; set; }
+        
     }
 }
