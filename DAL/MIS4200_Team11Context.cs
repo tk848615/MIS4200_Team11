ec5a4e9fe90cfe8fe7f738b2155573d3207a418b
Tim Kieltyka <tk848615@ohio.edu>
2020-03-02 11:42:25 -0500

    Work 3-2-20

---
+++ DAL/MIS4200_Team11Context.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using MIS4200_Team11.Models;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace MIS4200_Team11.DAL
+{
+    public class MIS4200_Team11Context : DbContext
+    {
+        public MIS4200_Team11Context() : base("name=DefaultConnection")
+        {
+           // Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200_Team11Context, MIS4200_Team11.Migrations.MISContext.Configuration>("DefaultConnection"));
+            // this method is a 'constructor' and is called when a new context is created
+            // the base attribute says which connection string to use
+        }
+        public DbSet<UserDetails> UserDetails { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+
+
+
+    }
+}
---
+++ Models/UserDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MIS4200_Team11.Models
+{
+    public class UserDetails
+    {
+        [Required]
+        public Guid ID { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+        [Display(Name = "Primary Phone")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Office")]
+        public string Office { get; set; }
+        [Display(Name = "Current position")]
+        public string Position { get; set; }
+        [Display(Name = "Hire Date")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime hireDate { get; set; }
+        public string photo { get; set; }
+
+       // public ICollection<A> User { get; set; }
+
+    }
+}
